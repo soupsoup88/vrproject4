5240c6cf35fa1465a55c7db3e8bfcde284f2b31c
Yuzhu Dong <yuzhudong@gmail.com>
2016-04-27 10:49:07 -0400

    not known

---
+++ project4/Assets/CityScript/HospitalToDevelopedTown.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(col : Collision) {
+	switchToDevelopedTown();
+}
+
+function switchToDevelopedTown()
+{
+	PlayerPrefs.SetInt("city",3);
+	Application.LoadLevel (0);
+}
